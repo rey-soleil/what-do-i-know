3df9e23849f0519c3d3218df6a49a07a7a4adcc3
rey-soleil <68834278+rey-soleil@users.noreply.github.com>
2023-05-30 10:28:13 -0700

    fix(API): implement retry mechanism

--- utils/fetch-openai-data.ts
+++ utils/fetch-openai-data.ts
@@ -3,35 +3,59 @@ import {
   CreateChatCompletionResponse,
 } from "openai";
 
-export async function getAgentResponse(
-  messages: ChatCompletionResponseMessage[]
-) {
-  const agentResponse: CreateChatCompletionResponse = await fetch("/api/chat", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({ messages }),
-  }).then((response) => response.json());
+export async function getAgentResponse(messages: ChatCompletionResponseMessage[]) {
+  const maxRetries = 3;
+  let retryCount = 0;
 
-  const message = agentResponse.choices[0].message!;
-  return message;
-}
+  while (retryCount < maxRetries) {
+    try {
+      const agentResponse: CreateChatCompletionResponse = await fetch("/api/chat", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ messages }),
+      }).then((response) => response.json());
 
-export async function getAgentSummary(
-  messages: ChatCompletionResponseMessage[]
-) {
-  const agentResponse: CreateChatCompletionResponse = await fetch(
-    "/api/summary",
-    {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ messages }),
+      const message = agentResponse.choices[0].message!;
+      return message;
+    } catch (error) {
+      console.error(`Error querying the API: ${error}`);
+      retryCount++;
+      await sleep(1000); // Wait for 1 second before retrying
     }
-  ).then((response) => response.json());
+  }
 
-  const summary = agentResponse.choices[0].message?.content!;
-  return summary;
+  throw new Error(`Max retry limit exceeded. Unable to get agent response.`);
 }
+
+export async function getAgentSummary(messages: ChatCompletionResponseMessage[]) {
+  const maxRetries = 3;
+  let retryCount = 0;
+
+  while (retryCount < maxRetries) {
+    try {
+      const agentResponse: CreateChatCompletionResponse = await fetch("/api/summary", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ messages }),
+      }).then((response) => response.json());
+
+      const summary = agentResponse.choices[0].message?.content!;
+      return summary;
+    } catch (error) {
+      console.error(`Error querying the API: ${error}`);
+      retryCount++;
+      await sleep(1000); // Wait for 1 second before retrying
+    }
+  }
+
+  throw new Error(`Max retry limit exceeded. Unable to get agent summary.`);
+}
+
+function sleep(ms: number) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
