35adb65eb646d35038ba70e12e654dd48e139bae
rey-soleil <68834278+rey-soleil@users.noreply.github.com>
2023-05-30 10:33:43 -0700

    temp: make /chat sleep so i can test retry

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -57,7 +57,7 @@ export async function POST(request: Request) {
       messages,
     });
 
-    sleep(5000); // Wait for 5 seconds before responding
+    await sleep(5000); // Wait for 5 seconds before responding
 
     return new Response(JSON.stringify(completion.data), { status: 200 });
   } catch (error) {
