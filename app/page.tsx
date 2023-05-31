11bdfddd03df6ce50f9c0aa87fedde35ee025ded
rey-soleil <68834278+rey-soleil@users.noreply.github.com>
2023-05-30 18:13:39 -0700

    feat(welcome): add welcome dialog

--- app/page.tsx
+++ app/page.tsx
@@ -3,13 +3,16 @@
 import { useState } from "react";
 import Messenger from "./components/Messenger";
 import Summary from "./components/Summary";
+import WelcomeDialog from "./components/WelcomeDialog";
 
 export default function Home() {
   const [summary, setSummary] = useState<string>(
     "Start talking to the agent to get a summary of your conversation!"
   );
+
   return (
     <main className="flex h-full w-full flex-col md:flex-row">
+      <WelcomeDialog />
       <div className="h-full p-5 md:w-1/2">
         <Messenger setSummary={setSummary} />
       </div>
