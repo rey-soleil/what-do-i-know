2b91b276ecefa96ac3bf0a39a9d775264b7f7cc6
rey-soleil <68834278+rey-soleil@users.noreply.github.com>
2023-06-06 11:16:23 -0700

    style(messenger): add explanatory comments

--- app/components/EzraMessage.tsx
+++ app/components/EzraMessage.tsx
@@ -6,6 +6,11 @@ type EzraMessageProps = {
   message?: ChatCompletionResponseMessage;
 };
 
+/*
+ * This component renders Ezra's unique messages, which use the typewriter
+ * component to simulate typing. If no message is passed in (as in when we're
+ * fetching Ezra's message), then we just show the typing cursor.
+ */
 export default function EzraMessage({ message }: EzraMessageProps) {
   return (
     <div className="relative max-w-[90%] bg-cornsilk p-2 font-mono font-medium outline">
