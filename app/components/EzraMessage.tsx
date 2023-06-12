e8263c6aa7e535e583b4d57c2520fa15e542b025
rey-soleil <68834278+rey-soleil@users.noreply.github.com>
2023-06-12 10:52:42 -0700

    fix(cursor): remove after typewriter finished

--- app/components/EzraMessage.tsx
+++ app/components/EzraMessage.tsx
@@ -1,5 +1,5 @@
 import { ChatCompletionResponseMessage } from "openai";
-import TypewriterComponent from "typewriter-effect";
+import Typewriter from "typewriter-effect";
 import Feedback from "./Feedback";
 
 type EzraMessageProps = {
@@ -14,13 +14,19 @@ type EzraMessageProps = {
 export default function EzraMessage({ message }: EzraMessageProps) {
   return (
     <div className="relative max-w-[90%] bg-cornsilk p-2 font-mono font-medium outline">
-      <TypewriterComponent
-        options={{
-          strings: message?.content || "",
-          autoStart: true,
-          delay: 0,
-        }}
-      />
+      {message && (
+        <Typewriter
+          onInit={(typewriter) => {
+            typewriter
+              .changeDelay(25)
+              .typeString(message.content)
+              .start()
+              .callFunction(() => {
+                document.querySelector(".Typewriter__cursor")?.remove();
+              });
+          }}
+        />
+      )}
       {message && <Feedback message={message} />}
     </div>
   );
